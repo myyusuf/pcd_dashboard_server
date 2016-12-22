a90edbd34f378406542f3b28aad5adca483c0cbd
myyusuf <myyusuf1911@gmail.com>
2016-12-23 06:55:58 +0700

    Read excel

--- pcddashboard/src/main/java/id/co/wika/pcd/dashboard/controller/ProjectProgressController.java
+++ pcddashboard/src/main/java/id/co/wika/pcd/dashboard/controller/ProjectProgressController.java
@@ -2,10 +2,20 @@ package id.co.wika.pcd.dashboard.controller;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Iterator;
 import java.util.Map;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +47,9 @@ public class ProjectProgressController {
 //			dir.mkdirs();
 
 		// Create the file on server
-		File serverFile = new File("/Users/myyusuf/Documents/Test/pcd_data/test.docx");
+		String filePath = "/Users/myyusuf/Documents/Test/pcd_data/dpe_input.xlsx";
+		
+		File serverFile = new File(filePath);
 		if(!serverFile.exists()){
 			serverFile.createNewFile();
 		}
@@ -46,7 +58,104 @@ public class ProjectProgressController {
 		stream.write(bytes);
 		stream.close();
 		
+		try {
+			readExcelFile(filePath);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
         return ResponseHelper.responseSuccess();
     }
+
+	private void readExcelFile(String filePath) throws Exception {
+		FileInputStream file = new FileInputStream(new File(filePath));
+		
+		//Get the workbook instance for XLS file 
+		@SuppressWarnings("resource")
+//		HSSFWorkbook workbook = new HSSFWorkbook(file);
+		XSSFWorkbook workbook = new XSSFWorkbook (file);
+		
+
+		//Get first sheet from the workbook
+		XSSFSheet sheet = workbook.getSheet("INPUTAN");
+		
+		//Iterate through each rows from first sheet
+		Iterator<Row> rowIterator = sheet.iterator();
+		
+		int projectRowNum = 0;
+		String projectCode = "";
+		String projectName = "";
+		BigDecimal rkapOk = new BigDecimal("0.0");
+		BigDecimal rkapOp = new BigDecimal("0.0");
+		BigDecimal rkapLk = new BigDecimal("0.0");
+		
+		while(rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+			
+			if(row.getRowNum() > 9 && row.getRowNum() < 46){
+				
+				//Last row indicator
+				Cell cell2 = row.getCell(2);
+				if(cell2 != null){
+					switch (projectRowNum){
+						case 0:
+							projectCode = cell2.getStringCellValue();
+							
+							Cell cell4 = row.getCell(4);
+							projectName = cell4.getStringCellValue();
+							
+							Cell cell9 = row.getCell(9);
+							double rkapOkTmp = cell9.getNumericCellValue();
+							rkapOk = BigDecimal.valueOf(rkapOkTmp);
+							
+							Cell cell10 = row.getCell(10);
+							double rkapOpTmp = cell10.getNumericCellValue();
+							rkapOp = BigDecimal.valueOf(rkapOpTmp);
+							
+							Cell cell11 = row.getCell(11);
+							double rkapLkTmp = cell11.getNumericCellValue();
+							rkapLk = BigDecimal.valueOf(rkapLkTmp);
+							
+							break;
+						case 1:
+							
+							break;
+						case 2:
+							System.out.println("Project code : " + projectCode + 
+									", Project name :" + projectName +
+									", RKAP OK : " + rkapOk.toPlainString() +
+									", RKAP OP : " + rkapOp.toPlainString() +
+									", RKAP LK : " + rkapLk.toPlainString()
+									);
+							break;
+						case 3:
+							projectRowNum = -1;
+							projectCode = "";
+							projectName = "";
+							rkapOk = new BigDecimal("0.0");
+							rkapOp = new BigDecimal("0.0");
+							rkapLk = new BigDecimal("0.0");
+							break;
+					}	
+				}
+//				System.out.println("Project row num : " + projectRowNum);
+				projectRowNum++;
+			}
+			
+			//For each row, iterate through each columns
+//			Iterator<Cell> cellIterator = row.cellIterator();
+//			while(cellIterator.hasNext()) {
+//				
+//				Cell cell = cellIterator.next();
+//				
+//				System.out.print(cell.getStringCellValue() + "");
+//			}
+			
+		}
+		
+		file.close();
+		
+	}
 
 }
